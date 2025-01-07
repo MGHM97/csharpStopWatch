e407cd54d130e332f5caa64495340f7d9b869679
MGHM97 <mateusoliveira469@gmail.com>
2025-01-07 17:13:52 -0400

    count options menu

--- Program.cs
+++ Program.cs
@@ -4,11 +4,10 @@ using System.Threading;
 namespace csharpStopWatch {
   class Program {
     static void Main (string[] args) {
-      Start();
+      Menu();
     }
 
-    static void Start() {
-      int time = 10;
+    static void Start(int time) {
       int currentTime = 0;
 
       while (currentTime != time) {
@@ -18,6 +17,33 @@ namespace csharpStopWatch {
 
         Thread.Sleep(1000);
       }
+
+      Console.Clear();
+      Console.WriteLine("Count finished. Returning to menu");
+      Thread.Sleep(2500);
+      Menu();
+    }
+
+    static void Menu() {
+      Console.Clear();
+      Console.WriteLine("Would you like the count to be done in minutes or seconds? Base your choice on the example below:");
+      Console.WriteLine("'numeric value's (10s) -> To count 10 seconds");
+      Console.WriteLine("'numeric value'm (1m) -> To count 1 minute");
+      Console.WriteLine("'0s' or '0m' -> Exit app");
+
+      Console.WriteLine("----------/--------/--------");
+
+      Console.WriteLine("Select one of the options above: ");
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+
+      int timeTypeMultiplier = 1;
+
+      if (type == 'm') timeTypeMultiplier = 60;
+      if (time == 0) System.Environment.Exit(0);
+
+      Start(time * timeTypeMultiplier);
     }
   }
 }
